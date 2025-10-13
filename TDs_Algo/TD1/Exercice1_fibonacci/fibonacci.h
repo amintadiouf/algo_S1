302e1eb31a0231717211fb2a4cbf366bd8e0131f
amintadiouf <aminta.diouf@uha.fr>
2025-10-13 06:15:51 +0200

    Update fibonacci.h
    code du fichier d'en-tête fibonacci.h

--- TDs_Algo/TD1/Exercice1_fibonacci/fibonacci.h
+++ TDs_Algo/TD1/Exercice1_fibonacci/fibonacci.h
@@ -1,1 +1,13 @@
+#ifndef FIBONACCI_H
+#define FIBONACCI_H
 
+//Calcule du n-ième terme de la suite de Fibonacci en utilisant une approche récursive naïve.
+//Complexité: O(2^n), lent.
+long long fib_naive(int n);
+
+
+//Calcule du n-ième terme de la suite de Fibonacci en utilisant la programmation dynamique.
+//Complexité: O(n), plus rapide.
+long long fib_dynamique(int n, long long memo[]);
+
+#endif
