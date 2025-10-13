0b6dc03f3a65d2dd0769f6e0b008b1ae4c3b56b8
amintadiouf <aminta.diouf@uha.fr>
2025-10-13 06:23:42 +0200

    Create nombres_premiers.h
    code du fichier d'en-tête pour Nombres Premiers

---
+++ TDs_Algo/TD1/Exercice2_Nombres_Premiers/nombres_premiers.h
@@ -0,0 +1,11 @@
+#ifndef NOMBRES_PREMIERS_H
+#define NOMBRES_PREMIERS_H
+
+//compte le nombre de nombres premiers inférieurs à n en utilisant une méthode de vérification naïve pour chaque nombre.
+int compte_premiers_naif(int n);
+
+
+//compte le nombre de nombres premiers inférieurs à n en utilisant l'algorithme du Crible d'Ératosthène.
+int compte_premiers_efficace(int n);
+
+#endif
